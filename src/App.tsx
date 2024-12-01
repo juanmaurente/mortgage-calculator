2b9db749cbbfed41e35c98f08ebb9dfbce55db54
Juan <juanmaurente@gmail.com>
2024-12-01 12:00:49 +1100

    Completed final mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Form from './components/Form/Form';
+import ResultsDisplay from './components/Results/ResultsDisplay';
 
 function App() {
 	return (
 		<>
 			<div className='containerWrapper'>
 				<Form />
+				<ResultsDisplay />
 			</div>
 		</>
 	);
